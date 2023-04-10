4475168327378ad46d632c73750fe6674bba9f22
silvery107 <1079931505@qq.com>
2023-04-10 02:35:08 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='monorec',
+      version='1.0',
+      description='MonoRec Module',
+      url="https://github.com/silvery107/MonoRec",
+      packages=[],
+     )
